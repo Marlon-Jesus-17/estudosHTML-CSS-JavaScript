75d2e92b0f100a4d9b34bd7a49803a46e379c5e4
Marlon <maarlonsantos17@gmail.com>
2026-05-27 20:35:15 -0300

    Aula de condições(if/else) em javascript

---
+++ javascript/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60)
+    console.log(`Você ultrapassou a velocidade permitida, foi multado`)
+console.log(`Dirija sempre usando cinto de segurança`)
